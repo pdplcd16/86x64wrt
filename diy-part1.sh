83a997887de11f558cb3e96c01a5cd3bf96623ac
pdplcd16 <58948885+pdplcd16@users.noreply.github.com>
2021-11-03 19:25:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,3 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-git clone https://gitlab.com/_jason/openwrt-rtl8812bu-package.git package/kernel/rtl88x2bu/
